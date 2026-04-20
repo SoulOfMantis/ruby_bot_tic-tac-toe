9bc9e5f46d3199cba32f958fe7c59feaf91fefb3
Masha Ligorova <masaligorova@gmail.com>
2026-04-20 16:30:35 +0300

    Add modifications

--- tic_tac_toe/lib/tic_tac_toe/bot.rb
+++ tic_tac_toe/lib/tic_tac_toe/bot.rb
@@ -1,21 +1,24 @@
 require 'telegram/bot'
+require_relative 'logic'
 
 module TicTacToe
   class Bot
     def initialize(token)
       @token = token
-      @games = {} 
+      @games = {}
     end
 
     def run
       Telegram::Bot::Client.run(@token) do |bot|
-        puts "Бот успешно запущен"
         bot.listen do |rq|
           case rq
+
           when Telegram::Bot::Types::Message
             handle_start(bot, rq) if rq.text&.include?('/start')
+
           when Telegram::Bot::Types::CallbackQuery
             handle_move(bot, rq)
+
           end
         end
       end
@@ -25,13 +28,15 @@ module TicTacToe
 
     def handle_start(bot, rq)
       chat_id = rq.chat.id
-      
-      game = TicTacToe::GameState.new(rq.from.id)
-      game.chat_id = chat_id 
-      
+      game = TicTacToe::GameState.new
+
+      game.player_x = rq.from.id
+      game.name_x = rq.from.first_name
+      game.chat_id = chat_id
+
       msg = bot.api.send_message(
         chat_id: chat_id,
-        text: "Игра началась! ❌ ходит первым.",
+        text: "Игра началась! ❌: #{game.name_x}. Ждем хода ⭕...",
         reply_markup: render_keyboard(game.board)
       )
 
@@ -44,40 +49,68 @@ module TicTacToe
       game = @games[chat_id]
       return unless game
 
-      data = rq.data.split('_')
-      row, col = data[1].to_i, data[2].to_i
+      user_id = rq.from.id
 
-      if game.can_move?(rq.from.id, row, col)
-        game.make_move(row, col)
-        
+      if game.player_o.nil? && user_id != game.player_x
+        game.player_o = user_id
+        game.name_o = rq.from.first_name
+      end
+
+      unless game.valid_turn?(user_id)
+        bot.api.answer_callback_query(
+          callback_query_id: rq.id,
+          text: "Сейчас не ваш ход!",
+          show_alert: false
+        )
+        return
+      end
+
+      row, col = rq.data.split('_')[1..2].map(&:to_i)
+
+      if game.make_move(row, col)
         bot.api.edit_message_reply_markup(
           chat_id: game.chat_id,
           message_id: game.message_id,
           reply_markup: render_keyboard(game.board)
         )
 
-        check_winner(bot, game)
+        check_game_status(bot, game)
       end
-      
+
       bot.api.answer_callback_query(callback_query_id: rq.id)
     end
 
-    def check_winner(bot, game)
-      if game.winner || game.draw?
-        text = game.winner ? "Победил #{game.winner == 'X' ? '❌' : '⭕'}!" : "Ничья! 🤝"
-        bot.api.send_message(chat_id: game.chat_id, text: text)
-        @games.delete(game.chat_id)
+    def check_game_status(bot, game)
+      return unless game.winner
+
+      if game.winner == 'Draw'
+        text = "Игра окончена! Ничья 🤝"
       else
-        game.switch_player
+        win_name  = game.winner == 'X' ? game.name_x : game.name_o
+        lose_name = game.winner == 'X' ? game.name_o : game.name_x
+        win_icon  = game.winner == 'X' ? '❌' : '⭕'
+        lose_icon = game.winner == 'X' ? '⭕' : '❌'
+
+        text = "🎉 ИГРА ОКОНЧЕНА! 🎉\n\n" \
+               "🏆 Победитель: #{win_name} (#{win_icon})\n" \
+               "💀 Проигравший: #{lose_name} (#{lose_icon})"
       end
+
+      bot.api.send_message(chat_id: game.chat_id, text: text)
+      @games.delete(game.chat_id)
     end
 
     def render_keyboard(board)
-      kb = board.each_with_index.map do |row, r_idx|
-        row.each_with_index.map do |cell, c_idx|
-          Telegram::Bot::Types::InlineKeyboardButton.new(text: cell || " ", callback_data: "move_#{r_idx}_#{c_idx}")
+      kb = board.each_with_index.map do |row_array, row_idx|
+        row_array.each_with_index.map do |cell, col_idx|
+          label = cell.nil? ? " " : cell
+          Telegram::Bot::Types::InlineKeyboardButton.new(
+            text: label,
+            callback_data: "move_#{row_idx}_#{col_idx}"
+          )
         end
       end
+
       Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
     end
   end
--- tic_tac_toe/lib/tic_tac_toe/logic.rb
+++ tic_tac_toe/lib/tic_tac_toe/logic.rb
@@ -1,71 +1,56 @@
 module TicTacToe
-  class Logic
-    def self.create_board
-      Array.new(3) { Array.new(3, nil) }
+  class GameState
+    attr_accessor :board, :current_player, :player_x, :player_o, :name_x, :name_o, :winner, :chat_id, :message_id
+
+    def initialize
+      @board = Array.new(3) { Array.new(3, nil) }
+      @current_player = 'X'
+      @player_x = nil 
+      @player_o = nil 
+      @name_x = nil 
+      @name_o = nil 
+      @winner = nil
+      @chat_id = nil
+      @message_id = nil
     end
 
-    def self.check_winner(board)
+    def valid_turn?(user_id)
+      return false if @winner 
+      
+      if @current_player == 'X'
+        @player_x == user_id
+      else
+        if @player_o.nil? && user_id != @player_x
+          @player_o = user_id
+        end
+        @player_o == user_id
+      end
+    end
+
+    def make_move(row, col)
+      return false if @board[row][col] || @winner
+
+      @board[row][col] = @current_player
+      @winner = check_winner
+      @current_player = (@current_player == 'X' ? 'O' : 'X') unless @winner
+      true
+    end
+
+    def check_winner
       lines = []
-
-      board.each { |row| lines << row }
-
-      board.transpose.each { |col| lines << col }
-
-      lines << [board[0][0], board[1][1], board[2][2]]
-      lines << [board[0][2], board[1][1], board[2][0]]
+      3.times do |i|
+        lines << @board[i] 
+        lines << [@board[0][i], @board[1][i], @board[2][i]] 
+      end
+      lines << [@board[0][0], @board[1][1], @board[2][2]]
+      lines << [@board[0][2], @board[1][1], @board[2][0]]
 
       lines.each do |line|
         return 'X' if line.all? { |cell| cell == 'X' }
         return 'O' if line.all? { |cell| cell == 'O' }
       end
-
+      return 'Draw' if @board.flatten.none?(&:nil?)
       nil
-    end
-  end
-
-  class GameState
-    attr_accessor :board, :current_player, :players, :chat_id, :message_id
-
-    def initialize(first_player_id)
-      @board = Logic.create_board
-      @current_player = 'X'
-      @players = { 'X' => first_player_id, 'O' => nil }
-      @chat_id = nil
-      @message_id = nil
-    end
-
-    def join_second_player(user_id)
-      if @players['O'].nil? && user_id != @players['X']
-        @players['O'] = user_id
-      end
-    end
-
-    def current_player?(user_id)
-      @players[@current_player] == user_id
-    end
-
-    def cell_empty?(row, col)
-      @board[row][col].nil?
-    end
-
-    def make_move(row, col)
-      @board[row][col] = @current_player
-    end
-
-    def switch_player
-      @current_player = (@current_player == 'X' ? 'O' : 'X')
-    end
-
-    def winner
-      Logic.check_winner(@board)
-    end
-
-    def draw?
-      !@board.flatten.include?(nil)
-    end
-
-    def finished?
-      winner || draw?
     end
   end
 end
